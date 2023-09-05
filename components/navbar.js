701011833234588a637ce299432cc1a5e3bc388c
kenjitheman <kenjitheman@proton.me>
2023-09-05 07:36:45 +0300

    feat: udated navbar.js (works -> projects)

--- components/navbar.js
+++ components/navbar.js
@@ -78,7 +78,7 @@ const Navbar = (props) => {
           mt={{ base: 4, md: 0 }}
         >
           <LinkItem href="https://github.com/kenjitheman?tab=repositories" path={path}>
-            works
+            projects
           </LinkItem>
           <LinkItem href="https://t.me/kenjitheman" path={path}>
             contact
