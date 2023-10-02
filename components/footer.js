0d5d110fc6e99e83eaa57677beaa6df4df4c8b9c
kenjitheman <kenjitheman@proton.me>
2023-10-02 19:31:43 +0300

    chore: source code link added

--- components/footer.js
+++ components/footer.js
@@ -3,6 +3,12 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
+      <a href="https://github.com/kenjitheman/kenjitheman.dev">
+        <p>
+          source code
+        </p>
+      </a>
+      <br />
       &copy; {new Date().getFullYear()} kenjitheman | all rights reserved
     </Box>
   )
