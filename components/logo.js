ac91cc1bd9518041a3916bc7ed5b317effdc9416
kenjitheman <kenjitheman@proton.me>
2023-10-02 19:31:05 +0300

    chore: logo padding

--- components/logo.js
+++ components/logo.js
@@ -5,10 +5,10 @@ import styled from '@emotion/styled'
 
 const LogoBox = styled.span`
   font-weight: bold;
-  font-size: 18px;
+  font-size: 20px;
   display: inline-flex;
   align-items: center;
-  height: 30px;
+  height: 40px;
   line-height: 20px;
   padding: 10px;
 
